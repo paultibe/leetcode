c71a290e7cf7a5651c4dc131af519ee617c94acc
Paul Tiberghien <121990596+paultibe@users.noreply.github.com>
2025-01-26 20:03:49 -0800

    Time: 49 ms (76.41%), Space: 27.1 MB (7.32%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,12 +1,17 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        # brute force approach 
         maxProfit = 0
-        for i in range (1, len(prices)):
-            for j in range (i - 1, 0, -1):
-                diff = prices[i] - prices[j]
-                if (diff > maxProfit):
-                    maxProfit = diff
+
+        # sliding window approach
+        left = 0
+        right = 0
+
+        for right in range(len(prices)):
+            if prices[right] < prices[left]:
+                left = right
+            diff = prices[right] - prices[left]
+            if diff > maxProfit:
+                maxProfit = diff
 
         return maxProfit
         
