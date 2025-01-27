6c2d28999b7805da83f1b5dcfdfd0f2e323d0b33
Paul Tiberghien <121990596+paultibe@users.noreply.github.com>
2025-01-26 20:06:26 -0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,17 +1,6 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        maxProfit = 0
-
-        # sliding window approach
-        left = 0
-        right = 0
-
-        for right in range(len(prices)):
-            if prices[right] < prices[left]:
-                left = right
-            diff = prices[right] - prices[left]
-            if diff > maxProfit:
-                maxProfit = diff
+        maxProfit = 0 # TEST
 
         return maxProfit
         
