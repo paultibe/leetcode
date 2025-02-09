32f414ad3f766d33423f9d695d98ae2e0ca4748c
Paul Tiberghien <121990596+paultibe@users.noreply.github.com>
2025-02-08 20:06:57 -0800

    Time: 7 ms (53.65%), Space: 24.2 MB (11.75%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,19 +1,33 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        # sorting
+        # bucket sort
+        # get counts first
+        # 5: 3
+        # 2: 3
+        # 1: 8
+        # 4: 7
         counts = {}
-        for i in range(len(nums)):
-            counts[nums[i]] = counts.get(nums[i], 0) + 1
+        for num in nums:
+            counts[num]= counts.get(num, 0) + 1 # can throw KeyError. use TryGetValue
+        
+        # place into buckets. dictionary with counts as keys, array of elements as values
+        # 3: 5, 2
+        # 7: 4
+        # 8: 1
+        buckets = [[] for i in range(len(nums) + 1)]
+        for num, cnt in counts.items():
+            buckets[cnt].append(num)
+
+        # extract values out of dictionary based on keys
+        # dict.keys() -> 3, 7, 8
         result = []
-        for i, cnt in counts.items():
-            result.append((cnt, i))
-        
-        result.sort() # (1, 5), (2, 3), (4, 8)
-        result2 = []
-        for i in range(k):
-            result2.append(result.pop()[1])
-        
-        return result2
+        for i in range(len(buckets)): # need to iterate back across entire list if all elements appear only once
+            values = buckets.pop()
+            while values:
+                result.append(values.pop())
+                if len(result) == k:
+                    return result
+
 
 
 
