3e45a8c02db52ad50972383be257915cc3588831
Paul Tiberghien <121990596+paultibe@users.noreply.github.com>
2025-02-09 09:09:01 -0800

    Time: 15 ms (31.47%), Space: 17.8 MB (79.32%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -1,23 +1,23 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         result = []
-        def combination(selected: List[int], options: List[int]):
-            if sum(selected) == target:
+        def combination(selected: List[int], options: List[int], total: int):
+            if total == target:
                 result.append(selected.copy())
                 return
-            if len(options) == 0 or sum(selected) > target:
+            if len(options) == 0 or total > target:
                 return
             # make choice
             choice = options.copy().pop()
             selected.append(choice)
             # call with choice (it still has the option available)
             copy = options.copy()
-            combination(selected, copy)
+            combination(selected, copy, total + choice)
             # afterwards, undo choice
             # if choice has been chosen, don't choose again
             selected.pop()
             options.pop()
-            combination(selected, options)
-        combination([], candidates)
+            combination(selected, options, total)
+        combination([], candidates, 0)
         return result
         
