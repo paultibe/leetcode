45bf831b972c296749bc286e9ddf5add264a8e08
Paul Tiberghien <121990596+paultibe@users.noreply.github.com>
2025-02-09 08:16:07 -0800

    Time: 4 ms (71.09%), Space: 19.4 MB (18%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,10 +1,17 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        # create mappings of values -> counts
-        counts = Counter(nums)
-        # for num in nums:
-        # counts[num] = counts.get(num, 0) + 1
-        sorted_items = sorted(list(counts.items()),key = lambda x: x[1])
+        # BOYER-MOORE. not intuitive
 
-        return sorted_items.pop()[0]
+        count = 1
+        candidate = nums[0]
 
+        for num in nums:
+            if num == candidate:
+                count += 1
+            else:
+                count -= 1
+                if count == 0:
+                    candidate = num
+                    count = 2
+        
+        return candidate 
