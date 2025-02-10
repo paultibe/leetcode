a31e5d45578c8c88018df22a83ac71ea07f3315f
Paul Tiberghien <121990596+paultibe@users.noreply.github.com>
2025-02-09 20:15:22 -0800

    Time: 70 ms (41.9%), Space: 18.3 MB (6.46%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.py
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.py
@@ -4,13 +4,13 @@ class Solution:
         # logic 
         counts = Counter(s) # works with any iterable
         # move unique values to hashset
-        uniqueLetters = set()
-        for letter, count in counts.items():
-            if count == 1:
-                uniqueLetters.add(letter)
+        # uniqueLetters = set()
+        # for letter, count in counts.items():
+        #     if count == 1:
+        #         uniqueLetters.add(letter)
 
         for i in range(len(s)):
-            if s[i] in uniqueLetters:
+            if s[i] in counts and counts[s[i]] == 1:
                 return i
 
         # not found
