4024d27eca43febbc142a6353171da37a7f496e2
Paul Tiberghien <121990596+paultibe@users.noreply.github.com>
2025-03-02 08:23:01 -0800

    Time: 0 ms (100%), Space: 21.1 MB (27.71%) - LeetHub

---
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst-optimalDFS.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        # 1 ,2 , 3, 4
+        # result[k - 1]
+        result = []
+        nodesToTraverse = k
+        def inOrderDfs(root, nodesToTraverse):
+            # base case
+            if not root:
+                return 
+            inOrderDfs(root.left, nodesToTraverse)
+            result.append(root.val)
+            nodesToTraverse -= 1
+            if nodesToTraverse == 0:
+                return result[k - 1]
+            inOrderDfs(root.right, nodesToTraverse)
+        
+        inOrderDfs(root, nodesToTraverse)
+        return result[k - 1]
