4af73f157f16ecc13f13410623c6b3ac63b05738
Paul Tiberghien <121990596+paultibe@users.noreply.github.com>
2025-03-02 11:37:39 -0800

    Time: 1 ms (16.93%), Space: 17.8 MB (50.59%) - LeetHub

---
+++ 0993-cousins-in-binary-tree/0993-cousins-in-binary-tree-BFS.py
@@ -0,0 +1,34 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isCousins(self, root: Optional[TreeNode], x: int, y: int) -> bool:
+        # BFS
+        queue = deque() # (node, parent)
+        queue.append((root, None))
+        nodesSeen = {} # val -> (depth, parent)
+        currDepth = 0
+        while queue:
+            # process one level at a time
+            for _ in range(len(queue)):
+                # process
+                curr, parent = queue.popleft()
+                # add info about it
+                nodesSeen[curr.val] = (currDepth, parent)
+                # check if both nodes have been seen
+                if x in nodesSeen and y in nodesSeen:
+                    # check if same depth
+                    if nodesSeen[x][0] != nodesSeen[y][0] or nodesSeen[x][1] == nodesSeen[y][1]:
+                        return False
+                    return True 
+                if curr.left:
+                    queue.append((curr.left, curr))
+                if curr.right:
+                    queue.append((curr.right, curr))
+            # move to next level
+            currDepth += 1
+
+        
