915dd2ddb084d50d4816c2d5ab9114ab723b89de
Paul Tiberghien <121990596+paultibe@users.noreply.github.com>
2025-03-02 23:25:29 -0800

    Time: 1677 ms (5.3%), Space: 19.1 MB (91.74%) - LeetHub

---
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow-bruteForceDFSIterative.py
@@ -0,0 +1,37 @@
+class Solution:
+    def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
+        ROWS, COLS = len(heights), len(heights[0])
+        directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+        
+        def bfs(r, c, prevVal):
+            nonlocal pacific, atlantic
+            queue = deque()
+            queue.append((r, c, float('inf')))
+            visited = set()
+            while queue:
+                for i in range(len(queue)):
+                    row, col, prev = queue.pop()
+                    if row < 0 or col < 0:
+                        pacific = True
+                        break
+                    if row >= ROWS or col >= COLS:
+                        atlantic = True
+                        break
+                    if heights[row][col] > prev or (row, col) in visited:
+                        break
+                    
+                    visited.add((row, col))
+                    for dx, dy in directions:
+                        queue.append((row + dx, col + dy, heights[row][col]))
+                        if pacific and atlantic:
+                            break
+        
+        res = []
+        for r in range(ROWS):
+            for c in range(COLS):
+                pacific = False
+                atlantic = False
+                bfs(r, c, float('inf'))
+                if pacific and atlantic:
+                    res.append([r, c])
+        return res
