da00e57de450a7fa567a28d1e7aeaad7d127e5b9
Paul Tiberghien <121990596+paultibe@users.noreply.github.com>
2025-03-04 16:30:12 -0800

    Time: 2 ms (84.55%), Space: 19.4 MB (19.97%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,17 +1,7 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        # BOYER-MOORE. not intuitive
-
-        count = 1
-        candidate = nums[0]
-
-        for num in nums:
-            if num == candidate:
-                count += 1
-            else:
-                count -= 1
-                if count == 0:
-                    candidate = num
-                    count = 2
-        
-        return candidate 
+        # NAIVE SOLUTION
+        counts = Counter(nums) # could use defaultdict(lambda: 0)
+        # gives you key value pairs (as tuples) and then sort
+        sortedCounts = sorted(counts.items(), key = lambda x: x[1]) # sort based on counts NOT numbers themselves
+        return sortedCounts.pop()[0]
