24ec9bcf9300ea927cfa9ed1b353238a758c3c5f
Paul Tiberghien <121990596+paultibe@users.noreply.github.com>
2026-01-05 14:11:23 -0800

    Time: 247 ms (52.9%), Space: 19.7 MB (91.08%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,40 +1,24 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        # bfs
-        ROWS = len(grid)
-        COLS = len(grid[0])
-        # top, left, right, bottom
-        directions = [(-1, 0), (0, -1), (0, 1), (1, 0)]
-        result = 0
-        visit = set() # (x, y)
-        # iterate through entire grid
-        def bfs(cell):
-            # todo list
-            queue = deque()
-            queue.append(cell)
-            while queue:
-                row, col = queue.popleft()
-                for direction in directions:
-                    # obtain each neighbour
-                    newRow = row + direction[0]
-                    newCol = col + direction[1]
-                    newCell = (newRow, newCol)
-                    if (newCell not in visit
-                        and newRow < ROWS
-                        and newCol < COLS
-                        and newCol >= 0
-                        and newRow >= 0
-                        and grid[newRow][newCol] == "1"):
-                            queue.append(newCell)
-                            visit.add(newCell)
-            
-        for row in range(ROWS):
-            for col in range(COLS):
-                if (grid[row][col] == "1" and (row, col) not in visit):
-                    bfs((row, col))
-                    result += 1
-        
-        return result
+        directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+        ROWS, COLS = len(grid), len(grid[0])
+        islands = 0
 
+        def dfs(r, c):
+            grid[r][c] = "0"
+            for dr, dc in directions:
+                newRow = dr + r
+                newCol = dc + c
+                notAnIsland = (newRow < 0 or newCol < 0 or newRow >= ROWS or
+                newCol >= COLS or grid[newRow][newCol] == "0")
+                if notAnIsland:
+                    continue
+                dfs(newRow, newCol)
 
+        for r in range(ROWS):
+            for c in range(COLS):
+                if grid[r][c] == "1":
+                    dfs(r, c)
+                    islands += 1
 
+        return islands
