064e6377ee5ee05a78a2074e42feda98038f3ae4
Paul Tiberghien <121990596+paultibe@users.noreply.github.com>
2026-01-11 11:50:10 -0800

    Time: 7 ms (76.4%), Space: 19.4 MB (6.93%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.py
+++ 0038-count-and-say/0038-count-and-say.py
@@ -1,25 +1,21 @@
 class Solution:
     def countAndSay(self, n: int) -> str:
-        # Base Case
         if n == 1:
             return "1"
         
-        # Recursive Step: Get the string from the previous step
         prev = self.countAndSay(n - 1)
         
-        # Build the current string based on the previous result
-        result = []
+        result = ""
         i = 0
         while i < len(prev):
+            # count
             count = 1
-            # Count how many times the same character repeats
             while i + 1 < len(prev) and prev[i] == prev[i + 1]:
                 i += 1
                 count += 1
             
-            # Append "count" followed by "digit"
-            result.append(str(count))
-            result.append(prev[i])
+            # say
+            result += str(count) + prev[i]
             i += 1
             
-        return "".join(result)
+        return result
