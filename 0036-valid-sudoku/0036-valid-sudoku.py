d2403c58b23d638e793bacbfe8ef7b5a4142efb5
Paul Tiberghien <121990596+paultibe@users.noreply.github.com>
2026-01-14 11:18:47 -0800

    Time: 3 ms (81.33%), Space: 19.5 MB (5.87%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,42 +1,48 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
+        # BRUTE FORCE
+        ROWS = 9
+        COLS = 9
+        SUBBOX_PER_ROW = 3
+        SUBBOX_PER_COL = 3
+        SUBBOX_SIZE = 3
+
         # validate rows
-        # keep a hashset for each row
-        BOARD_HEIGHT = 9
-        BOARD_WIDTH = BOARD_HEIGHT
-        SQUARE_HEIGHT = 3
-        SQUARE_WIDTH = SQUARE_HEIGHT
-        for row in range(BOARD_HEIGHT):
-            numbersSeenInRow = set()
-            for col in range(BOARD_WIDTH):
-                if board[row][col] == ".":
-                    continue
-                if board[row][col] in numbersSeenInRow:
+        for row in range(ROWS):
+            seen = set()
+            for col in range(COLS):
+                if board[row][col] in seen:
                     return False
-                numbersSeenInRow.add(board[row][col])
-        # validate columns
-        for col in range(BOARD_WIDTH):
-            numbersSeenInCol = set()
-            for row in range(BOARD_HEIGHT):
-                if board[row][col] == ".":
-                    continue
-                if board[row][col] in numbersSeenInCol:
+                if board[row][col] != ".":
+                        seen.add(board[row][col])
+
+        # validate cols
+        for col in range(COLS):
+            seen = set()
+            for row in range(ROWS):
+                if board[row][col] in seen:
                     return False
-                numbersSeenInCol.add(board[row][col])
-                
-        # validate 3x3 squares
-        for square in range(BOARD_HEIGHT):
-            numbersSeenInSquare = set()
-            for i in range(SQUARE_HEIGHT):
-                for j in range(SQUARE_WIDTH):
-                    row = i + (SQUARE_HEIGHT * (square // 3))
-                    col = j + (SQUARE_WIDTH * (square % 3))
-                    if board[row][col] == ".":
-                        continue
-                    if board[row][col] in numbersSeenInSquare:
-                        return False
-                    numbersSeenInSquare.add(board[row][col])
+                if board[row][col] != ".":
+                        seen.add(board[row][col])
         
-        # board is valid!
+        def isValidSubbox(topLeftRow, topLeftCol):
+            seen = set()
+            for row in range(topLeftRow, topLeftRow + SUBBOX_SIZE):
+                for col in range(topLeftCol, topLeftCol + SUBBOX_SIZE):
+                    if board[row][col] in seen:
+                        return False
+                    if board[row][col] != ".":
+                        seen.add(board[row][col])
+            return True
+
+        # validate subgrids
+        # 0,0 0,3 0,6
+        for i in range(SUBBOX_PER_ROW):
+            for j in range(SUBBOX_PER_COL):
+                topLeftRow = i * 3
+                topLeftCol = j * 3
+                if not isValidSubbox(topLeftRow, topLeftCol):
+                    return False
+        
         return True
-        # TODO: what if square is blank ("")
+
