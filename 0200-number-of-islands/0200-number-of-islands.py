9475916b41fd9b91d1e6bfd2127cad43cf9c656d
Paul Tiberghien <121990596+paultibe@users.noreply.github.com>
2026-01-14 12:16:43 -0800

    Time: 235 ms (78%), Space: 21.6 MB (41.24%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,24 +1,30 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+        if not grid:
+            return 0
+            
+        directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
         ROWS, COLS = len(grid), len(grid[0])
         islands = 0
-
-        def dfs(r, c):
-            grid[r][c] = "0"
-            for dr, dc in directions:
-                newRow = dr + r
-                newCol = dc + c
-                notAnIsland = (newRow < 0 or newCol < 0 or newRow >= ROWS or
-                newCol >= COLS or grid[newRow][newCol] == "0")
-                if notAnIsland:
-                    continue
-                dfs(newRow, newCol)
+        stack = [] # one big stack
 
         for r in range(ROWS):
             for c in range(COLS):
                 if grid[r][c] == "1":
-                    dfs(r, c)
                     islands += 1
-
+                    stack.append((r, c))
+                    grid[r][c] = "0"
+                    
+                    while stack:
+                        curr_r, curr_c = stack.pop()
+                        
+                        for dr, dc in directions:
+                            nr, nc = curr_r + dr, curr_c + dc
+                            
+                            if (0 <= nr < ROWS and 
+                                0 <= nc < COLS and 
+                                grid[nr][nc] == "1"):
+                                
+                                grid[nr][nc] = "0"
+                                stack.append((nr, nc))
         return islands
