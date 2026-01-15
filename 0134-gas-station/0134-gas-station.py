36177ce9d0b5f6960aff74cef6a66ea8365501c0
Paul Tiberghien <121990596+paultibe@users.noreply.github.com>
2026-01-15 09:04:15 -0800

    Time: 23 ms (57.95%), Space: 25.9 MB (16.57%) - LeetHub

--- 0134-gas-station/0134-gas-station.py
+++ 0134-gas-station/0134-gas-station.py
@@ -1,22 +1,13 @@
 class Solution:
     def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
         n = len(gas)
-        start = 0
-        end = 0
-        tank = 0
-        count = 0
-        
-        while count < n:
-            net = gas[end] - cost[end]
-            if tank + net >= 0:
-                tank += net
-                end += 1
-            else:
+        start, end = n - 1, 0
+        tank = gas[start] - cost[start]
+        while start > end: # exit: moving of start accounts for station pointed to by start
+            if tank < 0:
                 start -= 1
-                if start < 0:
-                    start = n - 1
-                tank += gas[start] - cost[start]
-                
-            count += 1
-            
+                tank += gas[start] - cost[start] # move first. like recursive call without lookahead
+            else:
+                tank += gas[end] - cost[end]
+                end += 1
         return start if tank >= 0 else -1
