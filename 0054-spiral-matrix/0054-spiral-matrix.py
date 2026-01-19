f81d7f17aebb9c8a1325d385e799fc2e753671a5
Paul Tiberghien <121990596+paultibe@users.noreply.github.com>
2026-01-19 14:40:56 -0800

    Time: 0 ms (100%), Space: 19.4 MB (9.52%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.py
+++ 0054-spiral-matrix/0054-spiral-matrix.py
@@ -2,28 +2,27 @@ class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
         m, n = len(matrix), len(matrix[0])
         res = []
-        
-        # clockwise
+        # Right, Down, Left, Up
         dirs = [(0, 1), (1, 0), (0, -1), (-1, 0)]
 
         def dfs(r, c, di):
-            if len(res) == m * n:
-                return
+            # 1. VALIDATION: Is this cell actually valid?
+            if not (0 <= r < m and 0 <= c < n and matrix[r][c] is not None):
+                return False # Signal that we hit a wall
             
+            # 2. ACTION: Process the cell
             res.append(matrix[r][c])
-            matrix[r][c] = "#"
+            matrix[r][c] = None # Mark visited
             
-            # try
+            # 3. RECURSION: Try to continue in the SAME direction
             dr, dc = dirs[di]
-            next_r, next_c = r + dr, c + dc
+            if not dfs(r + dr, c + dc, di):
+                # 4. ROTATION: If current direction failed, try the NEXT one
+                new_di = (di + 1) % 4
+                new_dr, new_dc = dirs[new_di]
+                dfs(r + new_dr, c + new_dc, new_di)
             
-            # if next not valid, rotate
-            if not (0 <= next_r < m and 0 <= next_c < n and matrix[next_r][next_c] is not "#"):
-                di = (di + 1) % 4
-                dr, dc = dirs[di]
-                next_r, next_c = r + dr, c + dc
-            
-            dfs(next_r, next_c, di)
+            return True
 
         dfs(0, 0, 0)
         return res
