50a640e83d7e0e6f20eef39de678982f77c52a6b
Paul Tiberghien <121990596+paultibe@users.noreply.github.com>
2026-01-19 14:47:05 -0800

    Time: 0 ms (100%), Space: 19.4 MB (9.52%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.py
+++ 0054-spiral-matrix/0054-spiral-matrix.py
@@ -1,28 +1,33 @@
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        if not matrix: return []
         m, n = len(matrix), len(matrix[0])
-        res = []
-        # Right, Down, Left, Up
-        dirs = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+        
+        # We need to keep track of visited cells without mutating the matrix
+        # Using a set to track visited coordinates
+        def solve(r, c, di, visited):
+            # 1. VALIDATION: If out of bounds or visited, this path is empty
+            if not (0 <= r < m and 0 <= c < n and (r, c) not in visited):
+                return []
 
-        def dfs(r, c, di):
-            # 1. VALIDATION: Is this cell actually valid?
-            if not (0 <= r < m and 0 <= c < n and matrix[r][c] is not None):
-                return False # Signal that we hit a wall
+            # 2. ACTION: Add current cell to visited for this branch
+            visited.add((r, c))
+            current_val = [matrix[r][c]]
             
-            # 2. ACTION: Process the cell
-            res.append(matrix[r][c])
-            matrix[r][c] = None # Mark visited
-            
-            # 3. RECURSION: Try to continue in the SAME direction
+            # Directions: Right, Down, Left, Up
+            dirs = [(0, 1), (1, 0), (0, -1), (-1, 0)]
             dr, dc = dirs[di]
-            if not dfs(r + dr, c + dc, di):
-                # 4. ROTATION: If current direction failed, try the NEXT one
+            
+            # 3. RECURSION: Try same direction first
+            res = solve(r + dr, c + dc, di, visited)
+            
+            # 4. ROTATION: If same direction returned nothing, try rotating
+            if not res:
                 new_di = (di + 1) % 4
                 new_dr, new_dc = dirs[new_di]
-                dfs(r + new_dr, c + new_dc, new_di)
-            
-            return True
+                res = solve(r + new_dr, c + new_dc, new_di, visited)
+                
+            # 5. RETURN: Current value + whatever the rest of the path found
+            return current_val + res
 
-        dfs(0, 0, 0)
-        return res
+        return solve(0, 0, 0, set())
