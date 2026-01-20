db1e4f58064a5b0d0f04c0ec5d6cd5233bda822c
Paul Tiberghien <121990596+paultibe@users.noreply.github.com>
2026-01-19 20:24:43 -0800

    Time: 1 ms (13.56%), Space: 19.4 MB (22%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,13 +1,15 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        result = [[]]
+        res = []
 
-        for num in nums:
-            # Create a 'snapshot' of the current result using list() or [:]
-            # This prevents the loop from seeing the subsets we are about to add
-            for i in range(len(result)):
-                subset = result[i]
-                new_subset = subset + [num] # Concise way to copy and add
-                result.append(new_subset)
-        
-        return result
+        def dfs(i, path):
+            if i >= len(nums):
+                res.append(path.copy())
+                return
+            path.append(nums[i])
+            dfs(i + 1, path)
+            path.pop()
+            dfs(i + 1, path)
+
+        dfs(0, [])
+        return res
