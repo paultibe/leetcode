0a5d6ced624d70a79b2f638dd7c28b99326c45f0
Paul Tiberghien <121990596+paultibe@users.noreply.github.com>
2026-01-26 18:13:08 -0800

    Time: 5385 ms (9.93%), Space: 100.2 MB (5.27%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -1,55 +1,20 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        """
-        scratch notes
-        checking if palindrome is substring: two cases if moving from middle or outside 
-        - from middle: if odd, just start pointers on adjacent characters
-        - from outside: end when pointers cross each other
-
-        brute force
-        TC: O(n^3)
-        SC: O(1)
-
-        improving on BF:
-        eg. "babaddaga"
-        substrings are composed of other substrings (subproblems)
-        many substrings may be composed of the same substring -> overlapping subproblems
-
-        """
+        resultLength = 0
+        resultLeft = 0
         cache = {}
 
-        def isPalindrome(left, right) -> bool:
-            # base
-            if left > right:
-                return True
+        def isPalindrome(left, right):
+            # if one or two characters, after first condition, you know you have palindrome
+            isPalindrome = s[left] == s[right] and ((right - left <= 2) or cache[(left + 1, right - 1)])
+            cache[(left, right)] = isPalindrome
+            return isPalindrome
 
-            # question
-            if (left, right) in cache:
-                return cache[(left, right)]
-            
-            # computation
-            if s[left] != s[right]:
-                return False
-                
-            # recursive call
-            cache[(left, right)] = isPalindrome(left + 1, right - 1)
-            return cache[(left, right)]
-                    
-        longestPalindromicSubstring = ""
-
-        for i in range(len(s)):
-            for j in range(i, len(s)):
+        for j in range(len(s)):
+            for i in range(j + 1):
                 if isPalindrome(i, j):
-                    if len(s[i:j+1]) > len(longestPalindromicSubstring):
-                        longestPalindromicSubstring = s[i:j+1]
-        
-        return longestPalindromicSubstring
-
-"""
-retro
-- you can always decompose problems into subproblems. this will always help.
-- but it's especially helpful when a problem has a recurrence relation. subproblems have same structure as parent problemm (not always the case)
-- overlapping subproblems = overlapping state space paths
-- if wm engaged, dry run does magical debugging.
-- could not catch single character cases. off by 1. pointers can't start different indices
-"""
+                    if (j - i + 1) > resultLength:
+                        resultLeft = i
+                        resultLength = j - i + 1
+                        
+        return s[resultLeft : resultLeft + resultLength]
