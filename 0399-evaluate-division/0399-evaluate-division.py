46334f2c46fbaea774e6d072f69189a067286186
Paul Tiberghien <121990596+paultibe@users.noreply.github.com>
2026-02-05 10:53:53 -0800

    Time: 0 ms (100%), Space: 19.6 MB (21.95%) - LeetHub

--- 0399-evaluate-division/0399-evaluate-division.py
+++ 0399-evaluate-division/0399-evaluate-division.py
@@ -1,31 +1,87 @@
 class Solution:
     def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
-        adj = collections.defaultdict(list)  # Map a -> list of [b, a/b]
+        """
+        scratch notes
+        -if query[i][0] or query[i][1] not in equations, return -1
+        a/b * b/c = a/c 
+        a/a = 1
+        searching for correct series of products of equations
+        example input: [a/b, a/e, a/d, b/c, b/d, b/e, e/j] looking for: a/c
+        first thought: 
+        - model each node as the ratio where a neighbour exists if node1.denominator = node2.numerator
+        - do multi-source dfs/bfs at each node where node.numerator = target.numerator
+        - stop search when node.denominator = target.denominator
+        - but what if you have input: [a/b, a/c] queries: [b/a, b/c]
+        - so then you need to add an extra node for each input equation
+        can model as a search in a graph where edge a -> b represents a/b
+        need to start at first value in query, end at second
 
-        for i, eq in enumerate(equations):
-            a, b = eq
-            adj[a].append((b, values[i]))
-            adj[b].append((a, 1 / values[i]))
+        - second thought
+        - edges are transitions between nodes
+        - so if each equation becomes an edge, then when we traverse a -> b -> c, we want product of edges
 
-        def bfs(src, target):
-            q, visit = deque([(src, 1)]), set()
-            visit.add(src)
+        approach
+        """
+        # 1. Define our nodes and their base values
+    # Each node is (numerator, denominator)
+        node_values = {}
+        adj = defaultdict(list)
+        variables = set()
 
-            while q:
-                node, w = q.popleft()
-                if node == target:
-                    return w
-                for nei, weight in adj[node]:
-                    if nei not in visit:
-                        visit.add(node)
-                        q.append((nei, w * weight))
-            return -1
+        for i, (num, den) in enumerate(equations):
+            val = values[i]
+            node_values[(num, den)] = val
+            node_values[(den, num)] = 1.0 / val
+            variables.add(num)
+            variables.add(den)
+
+        # 2. Build the adjacency list: Node A -> Node B
+        # Connection exists if A.denominator == B.numerator
+        all_nodes = list(node_values.keys())
+        for i in range(len(all_nodes)):
+            for j in range(len(all_nodes)):
+                u_num, u_den = all_nodes[i]
+                v_num, v_den = all_nodes[j]
+                if u_den == v_num:
+                    adj[all_nodes[i]].append(all_nodes[j])
+
+        def dfs(curr_node, target_den, visited, current_product):
+            if curr_node[1] == target_den:
+                return current_product
+            
+            visited.add(curr_node)
+            
+            for neighbor in adj[curr_node]:
+                if neighbor not in visited:
+                    # Multiply by the value of the 'neighbor' equation node
+                    result = dfs(neighbor, target_den, visited, current_product * node_values[neighbor])
+                    if result != -1.0:
+                        return result
+            return -1.0
+
+        results = []
+        for C, D in queries:
+            # Edge case: variable doesn't exist
+            if C not in variables or D not in variables:
+                results.append(-1.0)
+                continue
+            # Edge case: same variable
+            if C == D:
+                results.append(1.0)
+                continue
+                
+            found = False
+            # Multi-source DFS: Start at any node where numerator is C
+            for start_node in [n for n in all_nodes if n[0] == C]:
+                res = dfs(start_node, D, set(), node_values[start_node])
+                if res != -1.0:
+                    results.append(res)
+                    found = True
+                    break
+            
+            if not found:
+                results.append(-1.0)
+                
+        return results
+
         
-        result = []
-        for src, dest in queries:
-            if src not in adj or dest not in adj:
-                result.append(-1)
-            else:
-                result.append(bfs(src, dest))
-    
-        return result
