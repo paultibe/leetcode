2d013ea8876cba64133dd884ffd437b65a9be741
Paul Tiberghien <121990596+paultibe@users.noreply.github.com>
2026-02-08 15:36:56 -0800

    Time: 2 ms (26.14%), Space: 19.5 MB (24.96%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -1,13 +1,12 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
         cache = defaultdict(int)
+        cache[(m-1, n-1)] = 1
         for i in range(m-1, -1, -1):
             for j in range(n-1, -1, -1):
-                if i == (m - 1) and j == (n - 1):
-                    cache[(i,j)] = 1
-                    continue
                 right = cache[(i, j + 1)]
                 down = cache[(i + 1, j)]
-                cache[(i, j)] = right + down
+                # "+" for the m-1, n-1 case
+                cache[(i, j)] += right + down
 
         return cache[(0, 0)]
