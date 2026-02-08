c504d341d246d15d82fad3c74c62ce68d75ca176
Paul Tiberghien <121990596+paultibe@users.noreply.github.com>
2026-02-08 15:33:36 -0800

    Time: 0 ms (100%), Space: 19.5 MB (24.96%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -6,12 +6,8 @@ class Solution:
                 if i == (m - 1) and j == (n - 1):
                     cache[(i,j)] = 1
                     continue
-                right = 0
-                down = 0
-                if j < n - 1:
-                    right = cache[(i, j + 1)]
-                if i < m - 1:
-                    down = cache[(i + 1, j)]
+                right = cache[(i, j + 1)]
+                down = cache[(i + 1, j)]
                 cache[(i, j)] = right + down
 
         return cache[(0, 0)]
