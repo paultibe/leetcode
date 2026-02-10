2352c265c5f36dba979ce4c69a15d874eb4c1db0
Paul Tiberghien <121990596+paultibe@users.noreply.github.com>
2026-02-09 20:00:27 -0800

    Time: 69 ms (57.05%), Space: 19.7 MB (69.98%) - LeetHub

--- 0926-flip-string-to-monotone-increasing/0926-flip-string-to-monotone-increasing.py
+++ 0926-flip-string-to-monotone-increasing/0926-flip-string-to-monotone-increasing.py
@@ -1,5 +1,5 @@
 class Solution:
-    def minFlipsMonoIncr(self, s: str) -> int:
+    def minFlipsMonoIncr(self,s: str) -> int:
         # initially assume the pivot is at index -1 (everything should be 1)
         # suffix is everything after current element
         suffix_zeros = s.count('0')
@@ -10,12 +10,13 @@ class Solution:
         
         for char in s:
             if char == '0':
-                # no longer in the suffix; 
                 suffix_zeros -= 1
+                min_flips = min(min_flips, prefix_ones + suffix_zeros)
+                # post increment
+                
             else:
-                # current element is part of prefix
+                min_flips = min(min_flips, prefix_ones + suffix_zeros)
+                # post increment
                 prefix_ones += 1
                 
-            min_flips = min(min_flips, prefix_ones + suffix_zeros)
-            
         return min_flips
