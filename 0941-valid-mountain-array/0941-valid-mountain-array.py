f42b795fdabef6eada3c8f3f33498e534b147841
Paul Tiberghien <121990596+paultibe@users.noreply.github.com>
2026-02-22 18:07:46 -0800

    Time: 173 ms (6.71%), Space: 20.8 MB (16.12%) - LeetHub

---
+++ 0941-valid-mountain-array/0941-valid-mountain-array.py
@@ -0,0 +1,23 @@
+class Solution:
+    def validMountainArray(self, arr):
+        n = len(arr)
+        if n < 3:
+            return False
+
+        def find_peak(nums):
+            i = 0
+            while i + 1 < len(nums) and nums[i] < nums[i+1]:
+                i += 1
+            return i
+
+        def is_descending(nums, start_idx):
+            if start_idx == 0 or start_idx == len(nums) - 1:
+                return False
+            
+            for i in range(start_idx, len(nums) - 1):
+                if nums[i] <= nums[i+1]: # Must be strictly decreasing
+                    return False
+            return True
+
+        peak_index = find_peak(arr)
+        return is_descending(arr, peak_index)
