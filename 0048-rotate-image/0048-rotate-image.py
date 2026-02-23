41b1ff346f13c53e14a4c2aef5930db9ada3f6e4
Paul Tiberghien <121990596+paultibe@users.noreply.github.com>
2026-02-23 14:26:44 -0800

    Time: 0 ms (100%), Space: 19.3 MB (57.06%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.py
+++ 0048-rotate-image/0048-rotate-image.py
@@ -1,9 +1,9 @@
 class Solution:
     def rotate(self, matrix: List[List[int]]) -> None:
-        """
-        Do not return anything, modify matrix in-place instead.
-        """
-        rotated = [list(row[::-1]) for row in zip(*matrix)]
+        matrix.reverse()
+
+        rotated = zip(*matrix)
+        
+        # Clear and extend modifies the original object in-place
         matrix.clear()
         matrix.extend(rotated)
-        
