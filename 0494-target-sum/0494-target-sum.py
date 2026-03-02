558aae760418ecb303be99bfcfa459af3857dd9f
Paul Tiberghien <121990596+paultibe@users.noreply.github.com>
2026-03-02 15:54:06 -0800

    Time: 639 ms (5.01%), Space: 25.8 MB (43.12%) - LeetHub

--- 0494-target-sum/0494-target-sum.py
+++ 0494-target-sum/0494-target-sum.py
@@ -6,14 +6,19 @@ class Solution:
         if abs(target) > S: return 0
         n = len(nums)
 
+        # A single dictionary where keys are (current_sum, index)
         dp = defaultdict(int)
+
+        # Base Case: At the "back" (index n), 
+        # reaching the target sum is the only success.
         dp[(target, n)] = 1
 
-        for index in range(n - 1, -1, -1):
-            for current_sum in range(-S, S + 1):
-                add = dp[(current_sum + nums[index], index + 1)]
-                subtract = dp[(current_sum - nums[index], index + 1)]
-                
-                dp[(current_sum, index)] = add + subtract
+        # Build the table backwards
+        for i in range(n - 1, -1, -1):
+            for s in range(-S, S + 1):
+                # To calculate dp[(s, i)], we look at the results
+                # we already stored for index i + 1.
+                dp[(s, i)] = dp[(s + nums[i], i + 1)] + dp[(s - nums[i], i + 1)]
 
+        # The answer is starting at sum 0 and index 0
         return dp[(0, 0)]
